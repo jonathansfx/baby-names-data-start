cd76ab8e43056121230f4b656ea7c19b5cad8195
jonathan <97814319+jonathansfx@users.noreply.github.com>
2024-03-13 20:52:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# BABY NAMES DATA ASSIGNMENT START CODE
-
 import json
 
 
 def main():
-    # Load Baby Data from File
     file = open("baby-names-data.json")
     baby_data = json.load(file)
     file.close()
 
-    # Main Menu
     loop = True
     while loop:
         selection = getMenuSelection()
@@ -30,7 +26,6 @@ def main():
 
 
 def getMenuSelection():
-    # Print Menu & Return User Selection
     print("\n*** BABY DATA - MAIN MENU ***")
     print("* 1: Display All")
     print("* 2: Search by Gender")
@@ -43,29 +38,43 @@ def getMenuSelection():
 
 
 def displayAll(baby_data):
-    # Display All Baby Data
     print("\nDISPLAY ALL")
+    for entry in baby_data:
+        print(f"{entry['Name']} (Rank: {entry['Rank']}, Gender: {entry['Gender']})")
 
 
 def searchGender(baby_data):
-    # Dislay All Baby Names based on Gender
     print("\nSEARCH BY GENDER")
+    gender = input("Enter a gender (Boy/Girl): ").capitalize()
+    for entry in baby_data:
+        if entry['Gender'] == gender:
+            print(f"{entry['Name']} (Rank: {entry['Rank']}, Gender: {entry['Gender']})")
 
 
 def searchRank(baby_data):
-    # Display All Baby Names based on Rank
     print("\nSEARCH BY RANK")
+    min_rank = int(input("Enter a minimum rank: "))
+    max_rank = int(input("Enter a maximum rank: "))
+    for entry in baby_data:
+        if min_rank <= entry['Rank'] <= max_rank:
+            print(f"{entry['Name']} (Rank: {entry['Rank']}, Gender: {entry['Gender']})")
 
 
 def searchStartLetter(baby_data):
-    # Insert User Item into a Position
     print("\nSEARCH BY START LETTER")
+    start_letter = input("Enter a starting letter: ").capitalize()
+    for entry in baby_data:
+        if entry['Name'].startswith(start_letter):
+            print(f"{entry['Name']} (Rank: {entry['Rank']}, Gender: {entry['Gender']})")
 
 
 def searchNameLength(baby_data):
-    # Remove item from position
     print("\nSEARCH BY NAME LENGTH")
+    name_length = int(input("Enter a name length: "))
+    for entry in baby_data:
+        if len(entry['Name']) == name_length:
+            print(f"{entry['Name']} (Rank: {entry['Rank']}, Gender: {entry['Gender']})")
 
 
-# Invoke main to begin program
-main()
+if __name__ == "__main__":
+    main()
